0177e0e90bff0e834cbd08428147a7ce387ba660
Wilmar FS <wflorez52@gmail.com>
2025-04-26 11:56:07 -0500

    add models example file

---
+++ dialog-engine/app/models.py
@@ -0,0 +1,15 @@
+# Este es un ejemplo de como crear un modelo
+
+# from sqlalchemy import Column, Integer, String, Boolean
+# from sqlalchemy.sql.expression import text
+# from .database import Base
+# from sqlalchemy.sql.sqltypes import TIMESTAMP
+
+
+# class User(Base):
+#   __tablename__ = "users"
+  
+#   id = Column(Integer, primary_key=True, nullable=False)
+#   email = Column(String, nullable=False, unique=True)
+#   password = Column(String, nullable=False)
+#   created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"))

a71e5216f1151a3a07ce26454995c9fb3a3a7b15
Wilmar FS <wflorez52@gmail.com>
2025-04-26 12:40:58 -0500

    add user router to app

--- dialog-engine/app/main.py
+++ dialog-engine/app/main.py
@@ -1,8 +1,10 @@
-from typing import Union
 from fastapi import FastAPI
+from . import models
+from .database import engine
+from .routes import user
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-  return {"Hello": "World"}
+app.include_router(user.router)

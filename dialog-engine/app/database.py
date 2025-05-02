ede07817016760bedec0a4f743bdcaea063289cf
Wilmar FS <wflorez52@gmail.com>
2025-05-02 12:02:13 -0500

    add env variables

--- dialog-engine/app/database.py
+++ dialog-engine/app/database.py
@@ -3,19 +3,20 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import OperationalError
 import sys
+from .config import settings
 
-SQLALCHEMY_DATABASE_URL = "postgresql://<user>:<password>@<server>/<db_name>"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.db_username}:{settings.db_password}@{settings.db_hostname}:{settings.db_port}/{settings.db_name}"
 
 try:
-  engine = create_engine(SQLALCHEMY_DATABASE_URL)
-  
-  with engine.connect() as conn:
-    print("Conexión exitosa a la base de datos")
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+    with engine.connect() as conn:
+        print("Conexión exitosa a la base de datos")
 except OperationalError as e:
-  print("Error: Falló la conexión a la base de datos")
-  print("Detalle:",e)
-  sys.exit(1)
-  
+    print("Error: Falló la conexión a la base de datos")
+    print("Detalle:",e)
+    sys.exit(1)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()

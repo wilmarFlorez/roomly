6116e58e2a1719f0d20c8f956cfbdc937fbc0525
Wilmar FS <wflorez52@gmail.com>
2025-05-03 14:54:27 -0500

    restore database config

--- dialog-engine/app/main.py
+++ dialog-engine/app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
-from .routes import whatsapp
+from . import models
+from .database import engine
+from .routes import user, whatsapp
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
+app.include_router(user.router)
 app.include_router(whatsapp.router)

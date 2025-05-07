0ceaf477268e403af66463a1604b21aefe48f9ba
Wilmar FS <wflorez52@gmail.com>
2025-05-07 18:08:01 -0500

    updated create user

--- dialog-engine/app/routes/user.py
+++ dialog-engine/app/routes/user.py
@@ -1,5 +1,5 @@
 from fastapi import status, HTTPException, Depends, APIRouter
-from .. import schemas, utils
+from .. import schemas
 from ..models import models
 from sqlalchemy.orm import Session
 from ..database import get_db
@@ -11,10 +11,12 @@ router = APIRouter(prefix="/users")
     "/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserResponse
 )
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    hashed_password = utils.hash_password(user.password)
-    user.password = hashed_password
-
-    new_user = models.User(email=user.email, password=user.password)
+    new_user = models.User(
+        name=user.name,
+        phone_number=user.phone_number,
+        phone_number_id=user.phone_number_id,
+        email=user.email,
+    )
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
@@ -22,7 +24,7 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return new_user
 
 
-@router.get("/users/{id}", response_model=schemas.UserOut)
+@router.get("/users/{id}", response_model=schemas.UserResponse)
 def get_user(id: int, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
     if not user:
